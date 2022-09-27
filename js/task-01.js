7947dad7da5b68c53c41372c4c8e87394d5fadde
Iryna <106107121+IrynaLeman@users.noreply.github.com>
2022-09-27 15:25:10 +0300

    task-1

--- js/task-01.js
+++ js/task-01.js
@@ -17,8 +17,34 @@
 //Elements: 5
 
 const categories = document.querySelectorAll('.item');
-//console.log(categories); 
-
 console.log('Number of categories:', categories.length);
 
-console.log();
+const titleCategoryEl = document.querySelectorAll('h2');
+//console.log(titleCategoryEl[0]);
+//console.log(titleCategoryEl[1]);
+//console.log(titleCategoryEl[2]);
+
+
+const firstTitleCategoryEl = titleCategoryEl[0].textContent;
+const secondTitleCategoryEl = titleCategoryEl[1].textContent;
+const thirdTitleCategoryEl = titleCategoryEl[2].textContent;
+
+console.log('Category:', firstTitleCategoryEl);
+
+const firstCategoryListEl = titleCategoryEl[0].nextElementSibling;
+const firstCategoryListItemsEl = firstCategoryListEl.children;
+console.log('Elements:', firstCategoryListItemsEl.length);
+
+
+console.log('Category:', secondTitleCategoryEl);
+
+const secondCategoryListEl = titleCategoryEl[1].nextElementSibling;
+const secondCategoryListItemsEl = secondCategoryListEl.children;
+console.log('Elements:', secondCategoryListItemsEl.length);
+
+
+console.log('Category:', thirdTitleCategoryEl);
+
+const thirdCategoryListEl = titleCategoryEl[2].nextElementSibling;
+const thirdCategoryListItemsEl = thirdCategoryListEl.children;
+console.log('Elements:', thirdCategoryListItemsEl.length);
